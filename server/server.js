ed31451d876f038b347e5974563d597c06a2ee64
MrDoggie <wangjunyi@hotmail.com>
2018-03-17 16:43:11 -0700

    greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -14,10 +14,16 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newMessage',{
-    from: 'Argo',
-    text: 'Hey. What is going on.',
-    createdAt: 123123
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat',
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New User joined',
+    createdAt: new Date().getTime()
   });
 
   socket.on('createMessage', (message) => {
@@ -27,6 +33,13 @@ io.on('connection', (socket) => {
       text: message.text,
       createdAt: new Date().getTime()
     });
+
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
+
   });
 
   socket.on('disconnect', () => {
